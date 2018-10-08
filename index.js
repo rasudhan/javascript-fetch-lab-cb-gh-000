4f9d4a340b04dd431e8802d18e3eb0be30d58503
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 23:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const userName = 'rasudhan'
+const userName = ''
 const baseApi = 'https://api.github.com/'
 const fork = `${userName}/javascript-fetch-lab`
 
@@ -24,6 +24,21 @@ Repo.prototype.template = function(){
   return template;
 };
 
+//Create an issue through the Github API
+
+function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
+}
+
 //Fetch all issues through the Github API and display / append to the DOM
 
 function getIssues(data) {
@@ -41,27 +56,12 @@ function displayIssue(issue) {
   $('#issues').append(issue.template())
 }
 
+//Fetch and show repo info
 
-function createIssue() {
-  const issueTitle = document.getElementById('title').value
- const issueBody = document.getElementById('body').value
- const postData = { title: issueTitle, body: issueBody }
- fetch(`${baseApi}repos/${fork}/issues`, {
-   method: 'post',
-   headers: {
-     'Authorization': `token ${getToken()}`
-   },
-   body: JSON.stringify(postData)
- }).then(resp => getIssues())
-}
-
-function showResults(repo) {
-    $('#results').append(repo.template())
-}
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
   fetch(`${baseApi}repos/${repo}/forks`, {
     method: 'post',
     headers: {
@@ -71,11 +71,13 @@ function forkRepo() {
     let repo = new Repo(resp);
     showForkedRepo(repo);
   })
-
 }
 
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
+}
+
+
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return 'da809ca0c5a564bba745496754250edd1cc2099c'
 }
